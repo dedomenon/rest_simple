23b83e2dfb1db60c444e49c6653d97a6883f6a57
rb <rb@mdb.(none)>
2009-11-24 10:58:29 +0100

    corrected settings path in init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
 # Include hook code here
 require 'ostruct'
-::RestSimpleSettings = Madb::parse_settings('config/settings.yml')
+::RestSimpleSettings = Madb::parse_settings(File.dirname(__FILE__)+'/config/settings.yml')
+
 
 
 AppConfig.plugins.push( {:name => :rest_simple, :admin_entities_view => "/rest/simple/v1/admin/entities/show"  } )
