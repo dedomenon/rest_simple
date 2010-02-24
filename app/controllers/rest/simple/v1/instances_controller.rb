48394dca8b2e377ece300065018e3f3cac40f14c
rbauduin@myowndb.com <rbauduin@myowndb.com>
2010-02-24 19:21:58 +0100

    corrections for new entity

--- app/controllers/rest/simple/v1/instances_controller.rb
+++ app/controllers/rest/simple/v1/instances_controller.rb
@@ -30,24 +30,30 @@ class Rest::Simple::V1::InstancesController < ApplicationController
   def index
 
     @entity ||= Entity.find params["id"]
-    crosstab_result =  crosstab_query_for_entity(params["id"])
+    crosstab_result =  @entity.crosstab_query()
     if crosstab_result.nil?
       render :json => {} and return
     end
 
-    @details = build_details_hash
-    crosstab_query     = crosstab_result[:query]
-    crosstab_row =  CrosstabObject.connection.execute("select count(*) from #{crosstab_query} #{join_filters([crosstab_filter])}")[0]
-    crosstab_count = crosstab_row[0] ? crosstab_row[0] : crosstab_row['count']
+    @details = @entity.details_hash
 
-    @paginator = Paginator.new self, crosstab_count.to_i, list_length, page_number
-    limit, offset = @paginator.current.to_sql
 
-    query = "select * from #{crosstab_query}  #{join_filters([crosstab_filter])} order by \"#{order_by}\""
-    if params["format"]!="csv"
-      query += " limit #{limit} offset #{offset}"
-    end
-    @list = CrosstabObject.find_by_sql(query)
+    @list, @paginator = @entity.get_paginated_list(:filters =>  [crosstab_filter] , :format => params[:format], :highlight => params[:highlight], :default_page => params[list_id+"_page"] || ((params[:startIndex].to_i/list_length).ceil + 1) , :order_by => order_by, :direction => sort_direction, :list_length => list_length )
+
+
+
+#    crosstab_query     = crosstab_result[:query]
+#    crosstab_row =  CrosstabObject.connection.execute("select count(*) from #{crosstab_query} #{join_filters([crosstab_filter])}")[0]
+#    crosstab_count = crosstab_row[0] ? crosstab_row[0] : crosstab_row['count']
+#
+#    @paginator = Paginator.new self, crosstab_count.to_i, list_length, page_number
+#    limit, offset = @paginator.current.to_sql
+#
+#    query = "select * from #{crosstab_query}  #{join_filters([crosstab_filter])} order by \"#{order_by}\""
+#    if params["format"]!="csv"
+#      query += " limit #{limit} offset #{offset}"
+#    end
+#    @list = CrosstabObject.find_by_sql(query)
 
     respond_to do |format|
       format.json { 
@@ -86,4 +92,36 @@ class Rest::Simple::V1::InstancesController < ApplicationController
       params[:value_filter]= params[params[:value_filter_parameter_name]]
     end
   end
+
+
+
+#copied from entity_controller
+  def sort_direction
+    CrosstabObject.connection.quote_string(params[:dir].to_s)
+  end
+
+# returns a quoted version of the requestion column sorting
+   def order_by
+    session["list_order"]||={}
+    if params[:sort]
+      order=CrosstabObject.connection.quote_string(params[:sort].to_s)
+    elsif params[order_param] and ! params["highlight"] or params["highlight"]==""
+      order=CrosstabObject.connection.quote_string(params[order_param].to_s)
+      session["list_order"][list_id]=order
+    elsif session["list_order"].has_key? [list_id]
+      order = session["list_order"][list_id]
+    else
+      order = "id"
+    end
+    return order
+  end
+  def crosstab_filter
+    if detail_filter.nil?
+      return ""
+    else
+      detail = Detail.find detail_filter
+      return "\"#{CrosstabObject.connection.quote_string(detail.name.downcase)}\"::text ilike '#{leading_wildcard}#{CrosstabObject.connection.quote_string(params["value_filter"].to_s)}#{trailing_wildcard}'"
+    end
+  end
+
 end
